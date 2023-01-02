df81681a0a3af8d03a4f8247d10a44c319e04445
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2023-01-02 17:40:54 +0300

    Promo_progect

---
+++ src/js/script.js
@@ -0,0 +1,102 @@
+//adaptive selector
+jQuery(document).ready(function($) {
+    $('.promo').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+
+jQuery(document).ready(function($){
+    $('.header').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+
+jQuery(document).ready(function($){
+    $('header__logo').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+    $('.menu').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+    $('.menu__list').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+    $('.menu__item').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+
+jQuery(document).ready(function($){
+    $('.title__block').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+    $('.title__wrapper__market').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+    $('.title__wrapper__guarant').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+    $('.title__wrapper__calendar').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+    $('.title__wrapper__journey').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+
+jQuery(document).ready(function($){
+    $('.invite').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+
+jQuery(document).ready(function($){
+
+    $('.invite__text').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+jQuery(document).ready(function($){
+
+    $('.container').selectorQuery({
+        'widthStops': [320, 480, 640, 960, 1024, 1280], // An array of widths
+        'classPrefix': 'max-' // Prefix class names with this
+    });
+});
+
+    
+    
+    
+   
+   
+    
+
+
+
